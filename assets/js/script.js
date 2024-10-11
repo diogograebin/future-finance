2d82583ec39e9c2b8abbc41ab8ac0d3cde3cf394
Diogo Grings Graebin <diogograebin@gmail.com>
2024-10-11 10:51:09 -0300

    alterações de layout e nova função dentro do js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,23 @@ cards.forEach(card => {
         card.classList.remove('card-hover'); // Remove a classe de animação
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Captura o link de "Conteúdo"
+    const linkConteudo = document.getElementById('link-conteudo');
+    // Captura a seção "Conteúdo"
+    const secaoConteudo = document.getElementById('pagina-conteudo');
+    
+    // Adiciona um evento de clique no link
+    linkConteudo.addEventListener('click', function(event) {
+        // Previne o comportamento padrão do link
+        event.preventDefault();
+
+        // Realiza a rolagem suave até a seção "Conteúdo"
+        secaoConteudo.scrollIntoView({
+            behavior: 'smooth' // Suave rolagem
+        });
+    });
+});
+
