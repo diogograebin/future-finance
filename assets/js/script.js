b2462dbb066249ab1e500e065d45e636c49a1035
Diogo Grings Graebin <diogograebin@gmail.com>
2024-10-15 20:19:24 -0300

    criação da seção de conteúdo e seus módulos

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,24 @@ cards.forEach(card => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Captura o link de "Home"
+    const linkHome = document.getElementById('link-home');
+    // Captura a seção "Home"
+    const paginaInicial = document.getElementById('pagina1');
+    
+    // Adiciona um evento de clique no link
+    linkHome.addEventListener('click', function(event) {
+        // Previne o comportamento padrão do link
+        event.preventDefault();
+
+        // Realiza a rolagem suave até a seção "Home"
+        paginaInicial.scrollIntoView({
+            behavior: 'smooth' // Suave rolagem
+        });
+    });
+});
+
 
 document.addEventListener('DOMContentLoaded', function() {
     // Captura o link de "Conteúdo"
@@ -29,6 +47,38 @@ document.addEventListener('DOMContentLoaded', function() {
         // Realiza a rolagem suave até a seção "Conteúdo"
         secaoConteudo.scrollIntoView({
             behavior: 'smooth' // Suave rolagem
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Captura o link de "Sobre"
+    const linkSobre = document.getElementById('link-sobre');
+    // Captura a seção "Sobre"
+    const secaoCards = document.getElementById('cards-container');
+    
+    // Adiciona um evento de clique no link
+    linkSobre.addEventListener('click', function(event) {
+        // Previne o comportamento padrão do link
+        event.preventDefault();
+
+        // Realiza a rolagem suave até a seção "Sobre"
+        secaoCards.scrollIntoView({
+            behavior: 'smooth' // Suave rolagem
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const modulos = document.querySelectorAll('.modulo');
+
+    modulos.forEach(modulo => {
+        const titulo = modulo.querySelector('.titulo-modulo');
+        
+        titulo.addEventListener('click', function() {
+            // Adiciona ou remove a classe "active" no módulo clicado
+            modulo.classList.toggle('active');
         });
     });
 });
